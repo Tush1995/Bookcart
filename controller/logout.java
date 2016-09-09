43c0e073bfb8a2c7bc2ce1a7d6a8a6b81f5e9b18
tushar <tsonawane9011@gmail.com>
2016-09-09 09:56:35 +0530

    this is controller code

---
+++ controller/logout.java
@@ -0,0 +1,46 @@
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class logout
+ */
+@WebServlet("/logout")
+public class logout extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public logout() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out=response.getWriter();
+		response.setContentType("text/html");
+		HttpSession hs=request.getSession(false);
+		Object o=hs.getAttribute("unm");
+		hs.invalidate();
+		out.println("Logged Out Successfully");
+		RequestDispatcher rd=request.getRequestDispatcher("Login.html");
+		rd.include(request, response);
+		
+		
+	}
+
+}
